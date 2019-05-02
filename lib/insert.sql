f22131158de7c6c61f356d3b9612dd2cf04537b7
Kane Koo <kanekoo@gmail.com>
2019-05-01 23:08:51 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,67 @@
+INSERT INTO projects (id, title, category,funding_goal, start_date, end_date) VALUES
+(1,"Farmhub","agricultural", 200.00,'2019-06-01', '2019-06-15');
+INSERT INTO projects (id, title, category,funding_goal, start_date, end_date) VALUES
+(2,"Mark's Adventure School","physical fitness", 400.00,'2019-05-01', '2019-05-15');
+INSERT INTO projects (id, title, category,funding_goal, start_date, end_date) VALUES
+(3,"Monkey Sports","sporting goods", 600.00,'2019-04-01', '2019-04-15');
+INSERT INTO projects (id, title, category,funding_goal, start_date, end_date) VALUES
+(4,"Herman's","sporting goods", 800.00,'2019-07-01', '2019-07-15'),
+(5,"Steve's Sweet Shop","candy", 1000.00,'2019-08-01', '2019-08-15'),
+(6,"Clayton's Pharmacy","retail", 1200.00,'2019-09-01', '2019-09-15'),
+(7,"Device Shop","technical", 1400.00,'2019-07-01', '2019-07-15'),
+(8,"Egos R Us","services", 800.00,'2019-08-01', '2019-08-30'),
+(9,"Frank's Pizza","restaurant", 800.00,'2019-08-01', '2019-09-30'),
+(10,"Gus's Barbers","services", 1000.00,'2019-08-01', '2019-08-30');
+
+INSERT INTO users (id, name,age) VALUES
+(1,"Aaron",30),
+(2,"Ben",30),
+(3,"Clayton",30),
+(4,"Donald",31),
+(5,"Edward",32),
+(6,"Frank",30),
+(7,"Greta",30),
+(8,"Hannah",30),
+(9,"Irene",30),
+(10,"Jennifer",30),
+(11,"Karen",40),
+(12,"Lewis",40),
+(13,"Maria",40),
+(14,"Nunzio",40),
+(15,"Oren",40),
+(16,"Peter",40),
+(17,"Quinn",40),
+(18,"Ralph",40),
+(19,"Stan",40),
+(20,"Tim",40);
+INSERT INTO pledges VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 30.00, 1, 4),
+(4, 40.00, 2, 3),
+(5, 50.00, 3, 2),
+(6, 10.00, 4, 4),
+(7, 20.00, 5, 10),
+(8, 30.00, 6, 10),
+(9, 40.00, 7, 9),
+(10, 400.00, 8, 8),
+(11, 50.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
